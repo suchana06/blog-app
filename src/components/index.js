6fe72a66864d0476342f4cf3aaab544d690f521b
suchana06 <singhsuchana170@gmail.com>
2024-07-19 00:50:10 +0530

    adding login and signup pages

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,14 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import LogoutBtn from './Header/LogoutBtn'
 import Container from "./container/Container";
+import Button from './Button'
+import Input from "./Input";
 export {
     Header,
     Footer,
     Logo,
     LogoutBtn,
     Container,
-    
+    Button,
+    Input,
 }
